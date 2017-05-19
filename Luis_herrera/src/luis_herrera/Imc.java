ba2ce43931681c2bc49fa904a0250296005347c4
T- <T-@PC256>
2017-05-18 19:36:23 -0500

    Clase del 18/05/2017 Proyecto

---
+++ Luis_herrera/src/luis_herrera/Imc.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luis_herrera;
+
+/**
+ *
+ * @author T-
+ */
+public class Imc {
+    public float calcular(Usuario u){
+       // Lógica del calculo
+       float resultado=u.peso/(u.altura*u.altura);
+       
+       return resultado;
+        
+    }
+}
--- Luis_herrera/src/luis_herrera/Luis_herrera.java
+++ Luis_herrera/src/luis_herrera/Luis_herrera.java
@@ -8,6 +8,8 @@ package luis_herrera;
 /**
  *
  * @author T-
+ * apuntes:
+ * https://daton.github.io/juan-campos/  
  */
 public class Luis_herrera {
 
@@ -15,7 +17,25 @@ public class Luis_herrera {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Negrito chaka");
+        
+        // imc = w/(h.h) 
+        /*
+        Probar solución
+    Generando un usuario Hipotético
+        */
+        Usuario u=new Usuario();
+        
+        //asignamos valores
+        u.altura=1.365f;
+        u.peso=73f;
+        
+        //Generamos modelo
+        Imc imc=new Imc();
+        imc.calcular(u);
+        System.out.println("El valor es "+ imc.calcular(u));
+
     }
     
 }
